1abc57b013b883c5425b04d7e3f93f0cef08c304
Miguel Muscat <miguelmuscat93@gmail.com>
2018-02-21 18:03:59 +0100

    Added `BuildSqlRecordValuesCapableTrait`

---
+++ src/BuildSqlRecordValuesCapableTrait.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace RebelCode\Storage\Resource\Sql;
+
+use ArrayAccess;
+use Dhii\Util\String\StringableInterface as Stringable;
+use InvalidArgumentException;
+use OutOfRangeException;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use stdClass;
+use Traversable;
+
+/**
+ * Provides functionality for building record VALUES for INSERT SQL queries.
+ *
+ * @since [*next-version*]
+ */
+trait BuildSqlRecordValuesCapableTrait
+{
+    /**
+     * Builds the values for a single record.
+     *
+     * @since [*next-version*]
+     *
+     * @param array|Traversable                             $columns      The list of table columns.
+     * @param array|ArrayAccess|stdClass|ContainerInterface $record       The record data container.
+     * @param array                                         $valueHashMap Optional map of value names and their hashes.
+     *
+     * @return string The build row values as a comma separated list in parenthesis.
+     *
+     * @throws InvalidArgumentException    If the record data container is invalid.
+     * @throws ContainerExceptionInterface If an error occurred while reading from the record data container.
+     * @throws OutOfRangeException         If a column name is invalid.
+     */
+    protected function _buildSqlRecordValues($columns, $record, array $valueHashMap = [])
+    {
+        $data = [];
+
+        foreach ($columns as $_columnName) {
+            try {
+                // Get row data for this column
+                $_value = $this->_containerGet($record, $_columnName);
+                $_valueKey = $this->_normalizeString($_value);
+                // Use hash instead of value if available
+                $_realValue = isset($valueHashMap[$_valueKey])
+                    ? $valueHashMap[$_valueKey]
+                    : $this->_normalizeSqlValue($_value, $_columnName);
+            } catch (NotFoundExceptionInterface $notFoundException) {
+                $_realValue = 'DEFAULT';
+            }
+
+            $data[$_columnName] = $_realValue;
+        }
+
+        return sprintf('(%s)', implode(', ', $data));
+    }
+
+    /**
+     * Retrieves a value from a container or data set.
+     *
+     * @since [*next-version*]
+     *
+     * @param array|ArrayAccess|stdClass|ContainerInterface $container The container to read from.
+     * @param string|int|float|bool|Stringable              $key       The key of the value to retrieve.
+     *
+     * @throws InvalidArgumentException    If container is invalid.
+     * @throws ContainerExceptionInterface If an error occurred while reading from the container.
+     * @throws NotFoundExceptionInterface  If the key was not found in the container.
+     *
+     * @return mixed The value mapped to the given key.
+     */
+    abstract protected function _containerGet($container, $key);
+
+    /**
+     * Normalizes a value to its string representation.
+     *
+     * The values that can be normalized are any scalar values, as well as
+     * {@see StringableInterface).
+     *
+     * @since [*next-version*]
+     *
+     * @param string|int|float|bool|Stringable $subject The value to normalize to string.
+     *
+     * @throws InvalidArgumentException If the value cannot be normalized.
+     *
+     * @return string The string that resulted from normalization.
+     */
+    abstract protected function _normalizeString($subject);
+
+    /**
+     * Normalizes an SQL value, quoting it if it's a string.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|int|float|bool|Stringable $value  The input value.
+     * @param string|Stringable|null           $column Optional column name.
+     *
+     * @return string The normalized value.
+     */
+    abstract protected function _normalizeSqlValue($value, $column = null);
+}
---
+++ test/unit/BuildSqlRecordValuesCapableTraitTest.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace RebelCode\Storage\Resource\Sql\UnitTest;
+
+use Xpmock\TestCase;
+use RebelCode\Storage\Resource\Sql\BuildSqlRecordValuesCapableTrait as TestSubject;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class BuildSqlRecordValuesCapableTraitTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\Storage\Resource\Sql\BuildSqlRecordValuesCapableTrait';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return MockObject|TestSubject
+     */
+    public function createInstance()
+    {
+        $builder = $this->getMockBuilder(static::TEST_SUBJECT_CLASSNAME)
+                        ->setMethods(
+                            [
+                                '_containerGet',
+                                '_containerHas',
+                                '_normalizeString',
+                                '_normalizeSqlValue',
+                            ]
+                        );
+
+        $mock = $builder->getMockForTrait();
+        $mock->method('_normalizeString')->willReturnCallback(
+            function($input) {
+                return strval($input);
+            }
+        );
+
+        return $mock;
+    }
+
+    /**
+     * Creates a mock that both extends a class and implements interfaces.
+     *
+     * This is particularly useful for cases where the mock is based on an
+     * internal class, such as in the case with exceptions. Helps to avoid
+     * writing hard-coded stubs.
+     *
+     * @since [*next-version*]
+     *
+     * @param string   $className      Name of the class for the mock to extend.
+     * @param string[] $interfaceNames Names of the interfaces for the mock to implement.
+     *
+     * @return MockObject The object that extends and implements the specified class and interfaces.
+     */
+    public function mockClassAndInterfaces($className, $interfaceNames = [])
+    {
+        $paddingClassName = uniqid($className);
+        $definition = vsprintf(
+            'abstract class %1$s extends %2$s implements %3$s {}',
+            [
+                $paddingClassName,
+                $className,
+                implode(', ', $interfaceNames),
+            ]
+        );
+        eval($definition);
+
+        return $this->getMockBuilder($paddingClassName)->getMockForAbstractClass();
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInternalType(
+            'object',
+            $subject,
+            'An instance of the test subject could not be created'
+        );
+    }
+
+    /**
+     * Tests the SQL record values build method to assert whether the returned values string is correct.
+     *
+     * @since [*next-version*]
+     */
+    public function testBuildSqlRecordValues()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $val1 = rand(0, 50);
+        $val2 = uniqid('str-');
+        $val3 = rand(30, 100);
+        $hash1 = uniqid('hash-');
+        $hash2 = uniqid('hash-');
+        $hash3 = uniqid('hash-');
+
+        $columns = [
+            $col1 = uniqid('column-'),
+            $col2 = uniqid('column-'),
+            $col3 = uniqid('column-'),
+        ];
+        $record = [
+            $col1 => $val1,
+            $col2 => $val2,
+            $col3 => $val3,
+        ];
+
+        $valueHashMap = [
+            $val1 => $hash1,
+            $val2 => $hash2,
+            $val3 => $hash3,
+        ];
+
+        $subject->expects($this->exactly(count($columns)))
+                ->method('_containerGet')
+                ->withConsecutive([$record, $col1], [$record, $col2], [$record, $col3])
+                ->willReturnOnConsecutiveCalls($val1, $val2, $val3);
+
+        $expected = sprintf('(%1$s, %2$s, %3$s)', $hash1, $hash2, $hash3);
+        $actual = $reflect->_buildSqlRecordValues($columns, $record, $valueHashMap);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Tests the SQL record values build method to assert whether the returned values string is correct when a record
+     * is missing a value for a particular column.
+     *
+     * @since [*next-version*]
+     */
+    public function testBuildSqlRecordValuesMissingColumn()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $val1 = rand(0, 50);
+        $val2 = uniqid('str-');
+        $val3 = rand(30, 100);
+        $hash1 = uniqid('hash-');
+        $hash2 = uniqid('hash-');
+        $hash3 = uniqid('hash-');
+
+        $columns = [
+            $col1 = uniqid('column-'),
+            $col2 = uniqid('column-'),
+            $col3 = uniqid('column-'),
+        ];
+        $record = [
+            $col1 => $val1,
+            $col3 => $val3,
+        ];
+
+        $valueHashMap = [
+            $val1 => $hash1,
+            $val2 => $hash2,
+            $val3 => $hash3,
+        ];
+
+        $subject->expects($this->exactly(count($columns)))
+                ->method('_containerGet')
+                ->withConsecutive([$record, $col1], [$record, $col2], [$record, $col3])
+                ->willReturnCallback(
+                    function($c, $k) {
+                        if (isset($c[$k])) {
+                            return $c[$k];
+                        }
+
+                        throw $this->mockClassAndInterfaces('Exception', ['Psr\Container\NotFoundExceptionInterface']);
+                    }
+                );
+
+        $expected = sprintf('(%1$s, DEFAULT, %2$s)', $hash1, $hash3);
+        $actual = $reflect->_buildSqlRecordValues($columns, $record, $valueHashMap);
+
+        $this->assertEquals($expected, $actual);
+    }
+}
