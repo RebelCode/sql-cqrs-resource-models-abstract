9b4a15d7023035b74e7410474f5f6e70a612083f
Miguel Muscat <miguelmuscat93@gmail.com>
2018-04-10 14:28:44 +0200

    Added `BuildSqlFromCapableTrait`

---
+++ src/BuildSqlFromCapableTrait.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace RebelCode\Storage\Resource\Sql;
+
+use InvalidArgumentException;
+use OutOfRangeException;
+use Dhii\Util\String\StringableInterface as Stringable;
+use stdClass;
+use Traversable;
+
+/**
+ * Functionality for building the FROM portion of an SQL query.
+ *
+ * @since [*next-version*]
+ */
+trait BuildSqlFromCapableTrait
+{
+    /**
+     * Builds the SQL FROM section.
+     *
+     * @since [*next-version*]
+     *
+     * @param array|stdClass|Traversable $tables A mapping of tables names (keys) to their aliases (values).
+     *
+     * @return string The build SQL table FROM section.
+     */
+    protected function _buildSqlFrom($tables)
+    {
+        $parts = [];
+
+        foreach ($tables as $_table => $_alias) {
+            $parts[] = sprintf(
+                '%1$s as %2$s',
+                $this->_escapeSqlReference($_table),
+                $this->_escapeSqlReference($_alias)
+            );
+        }
+
+        $imploded = implode(', ', $parts);
+
+        return sprintf('FROM %s', $imploded);
+    }
+
+    /**
+     * Escapes an SQL reference with an optional prefix.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable      $reference The reference string.
+     * @param string|Stringable|null $prefix    The reference prefix, if any.
+     *
+     * @throws InvalidArgumentException If either argument is not a valid string.
+     * @throws OutOfRangeException      If an invalid string is given as argument.
+     *
+     * @return string The escaped string.
+     */
+    abstract protected function _escapeSqlReference($reference, $prefix = null);
+}
